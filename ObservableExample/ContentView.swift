02c09546bddb5447f0b02f9e5b0676943b29c38f
Antonio <antoniojesus.ib@gmail.com>
2023-10-12 21:12:25 +0200

    Initial Commit

---
+++ ObservableExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObservableExample
+//
+//  Created by Antonio Jesús on 12/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObservableExample/ObservableExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableExampleApp.swift
+//  ObservableExample
+//
+//  Created by Antonio Jesús on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ObservableExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
